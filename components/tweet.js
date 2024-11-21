ea328c3deb5a84f64e4c80c965fb9bbe01688d13
Clément N <nouet.clem@gmail.com>
2024-11-21 17:43:51 +0100

    fin de journée

--- components/tweet.js
+++ components/tweet.js
@@ -1,15 +1,14 @@
 import styles from '../styles/Tweet.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faTrash } from '@fortawesome/free-solid-svg-icons';
-import { useSelector } from 'react-redux';
 
 function Tweet(props)  {
-    const user = useSelector((state) => state.user.value); // Récupération de l'utilisateur depuis le Redux store
+
   return (
     <div className={styles.container}>
       <div className={styles.userInfos}>
         <img className={styles.images} src="/pp_dog.png" alt="Photo de profil" width="60px" height="60px"/>
-        <h2 className={styles.name}>{user.username}</h2>
+        <h2 className={styles.name}>{props.username}</h2>
         <p className={styles.date}>{props.createAt}</p>
       </div>
       <div className={styles.tweetContent}>
