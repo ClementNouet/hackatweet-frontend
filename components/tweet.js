3f30644357c65e277cf189b5ee50a6394555d674
Clément N <nouet.clem@gmail.com>
2024-11-21 17:34:54 +0100

    oui

--- components/tweet.js
+++ components/tweet.js
@@ -7,16 +7,17 @@ function Tweet(props)  {
     const user = useSelector((state) => state.user.value); // Récupération de l'utilisateur depuis le Redux store
   return (
     <div className={styles.container}>
-      <div>
-        <img className={styles.images} src="/pp_dog.png" alt="Photo de profil" />
+      <div className={styles.userInfos}>
+        <img className={styles.images} src="/pp_dog.png" alt="Photo de profil" width="60px" height="60px"/>
         <h2 className={styles.name}>{user.username}</h2>
-      </div>
-      <div>
-        <p>{props.content}</p>
         <p className={styles.date}>{props.createAt}</p>
       </div>
-      <div>
+      <div className={styles.tweetContent}>
+        <p>{props.content}</p>
+      </div>
+      <div className={styles.tweetIcons}>
         <FontAwesomeIcon icon={faHeart} />
+        <p>0</p>
         <FontAwesomeIcon icon={faTrash} />
       </div>
     </div>
