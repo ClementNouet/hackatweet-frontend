bb48485d91e1c70971487bd35540a6980fd101ee
Diego <gomeztorres.diegoandres@gmail.com>
2024-11-22 12:25:34 +0100

    cssr

--- components/tweet.js
+++ components/tweet.js
@@ -9,10 +9,18 @@ const url = process.env.NEXT_PUBLIC_BACK_URL
 function Tweet(props)  {
   const { removeTweet } = props
   const [likes, setLikes] = useState(props.likes);
+  const [isLiked, setIsLiked] = useState(false)
 
   const user = useSelector((state) => state.user.value); // Récupération de l'utilisateur depuis le Redux store
     const handleLike = () => {
-      const newLikes = likes + 1;
+      let newLikes = null;
+      if(isLiked){
+        newLikes = likes - 1
+        setIsLiked(false)
+      }else{
+        newLikes = likes + 1;
+        setIsLiked(true)
+      }
       setLikes(newLikes);
       fetch(`${url}/tweets/${props.id}`, {
         method: 'PUT',
@@ -34,8 +42,8 @@ function Tweet(props)  {
         <p>{props.content}</p>
       </div>
       <div>
-        <FontAwesomeIcon icon={faHeart} className={styles.icon} onClick={()=>handleLike()}/>
-        <span> {likes}  </span>
+        <FontAwesomeIcon icon={faHeart} className={styles.icon} style={{color: isLiked? "#ae445a" : "#FFFFFF"}} onClick={()=>handleLike()}/>
+        <span style={{color: isLiked? "#ae445a" : "#FFFFFF"}}> {likes}  </span>
 
             {props.token === user.token ? (
                 <FontAwesomeIcon icon={faTrash} className={styles.icon}  onClick={() => removeTweet(props.id)}/>
