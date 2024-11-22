b9c5eb9c076530022117df9015933c25b4b89024
Clément N <nouet.clem@gmail.com>
2024-11-22 16:30:50 +0100

    oui

--- components/tweet.js
+++ components/tweet.js
@@ -2,43 +2,59 @@ import styles from '../styles/Tweet.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import moment from 'moment'
+import { useSelector } from 'react-redux';
+import moment from 'moment';
 
-const url = process.env.NEXT_PUBLIC_BACK_URL
+const url = process.env.BACK_URL;
 
-function Tweet(props)  {
-  const { removeTweet } = props
+function Tweet(props) {
+  const { removeTweet } = props;
   const [likes, setLikes] = useState(props.likes);
-  const [isLiked, setIsLiked] = useState(false)
-  const [coeur, setCoeur] = useState(null)
+  const [isLiked, setIsLiked] = useState(false);
 
-  const user = useSelector((state) => state.user.value); // Récupération de l'utilisateur depuis le Redux store
-    const handleLike = () => {
-      const newLikes = isLiked ? likes - 1 : likes + 1; // Calcul du nouveau nombre de likes
-      const newIsLiked = !isLiked; // Inversion de l'état `isLiked`
+  const user = useSelector((state) => state.user.value);
 
-      setLikes(newLikes);
-      setIsLiked(newIsLiked);
-      fetch(`${url}/tweets/${props.id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({likes: newLikes, token: user.token, isLiked: newIsLiked}),
-        }).then(response => response.json())
-          .then((data)=>{
-            if (data.result){
-              setIsLiked(true)
-            }else{
-              setIsLiked(false)
-            }
-          });
+  // Charger l'état initial depuis le backend
+  useEffect(() => {
+    fetch(`${url}/tweets/${props.id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          setLikes(data.likes);
+          setIsLiked(data.isLiked); // Supposez que `isLiked` est retourné pour cet utilisateur
+        }
+      });
+  }, [props.id]);
+
+  const handleLike = () => {
+    let newLikes = likes;
+    if (isLiked) {
+      newLikes -= 1;
+      setIsLiked(false);
+    } else {
+      newLikes += 1;
+      setIsLiked(true);
     }
+    setLikes(newLikes);
 
+    // Mettre à jour le backend
+    fetch(`${url}/tweets/${props.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ likes: newLikes, userToken: user.token }),
+    });
+  };
 
-    return (
+  return (
     <div className={styles.container}>
       <div className={styles.userInfos}>
-        <img className={styles.images} src="/pp_dog.png" alt="Photo de profil" width="60px" height="60px"/>
+        <img
+          className={styles.images}
+          src="/pp_dog.png"
+          alt="Photo de profil"
+          width="60px"
+          height="60px"
+        />
         <h2 className={styles.name}>{props.username}</h2>
         <p className={styles.date}>{moment(props.createAt).fromNow()}</p>
       </div>
@@ -46,12 +62,21 @@ function Tweet(props)  {
         <p>{props.content}</p>
       </div>
       <div>
-        <FontAwesomeIcon icon={faHeart} className={styles.icon} style={{color: isLiked? "#ae445a" : "#FFFFFF"}} onClick={()=>handleLike()}/>
-        <span style={{color: isLiked? "#ae445a" : "#FFFFFF"}}> {likes}  </span>
+        <FontAwesomeIcon
+          icon={faHeart}
+          className={styles.icon}
+          style={{ color: isLiked ? '#ae445a' : '#FFFFFF' }}
+          onClick={() => handleLike()}
+        />
+        <span style={{ color: isLiked ? '#ae445a' : '#FFFFFF' }}>{likes}</span>
 
-            {props.token === user.token ? (
-                <FontAwesomeIcon icon={faTrash} className={styles.icon}  onClick={() => removeTweet(props.id)}/>
-            ) : null}
+        {props.token === user.token ? (
+          <FontAwesomeIcon
+            icon={faTrash}
+            className={styles.icon}
+            onClick={() => removeTweet(props.id)}
+          />
+        ) : null}
       </div>
     </div>
   );
